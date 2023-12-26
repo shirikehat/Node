ef758f1ebe71ea1001c28aecb12f486b08f710a4
User <User@LAB3-3>
2023-12-26 09:35:23 +0200

    Add project files.

---
+++ ConsoleApp1/Node.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Node<T>
+    {
+        private T value;
+        private Node<T> next;
+        public Node(T value)
+        {
+            this.value = value;
+            next = null;
+        }
+
+        public Node(T value, Node<T> node)
+        {
+            this.value= value;
+            this.next = next;
+        }
+
+        public T GetValue() { return value; }
+        public void SetValue(T value) { this.value = value;}
+        public Node<T> GetNext() { return next;}
+        public void SetNext(Node<T> next) {  this.next = next;}
+
+        public bool HasNext()
+        {
+            return this.next != null;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Specialized;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static int Count(Node<int> train)
+        {
+            int counter = 0;
+            while(train != null)
+            {
+                counter++;
+                train= train.GetNext();
+            }
+            return counter;
+        }
+
+        public static int CountRec(Node<int> train)
+        {
+            int counter = 0;
+            if (train != null) return 0;
+            
+            return 1+CountRec(train.GetNext());
+        }
+
+        public static void DoubleVal(Node<int> train)
+        {
+            while(train != null)
+            {
+                train.SetValue(train.GetValue() * 2);
+                train= train.GetNext();
+            }
+            
+        }
+
+        static void Main(string[] args)
+        {
+            Node<int> b = new Node<int>(4);
+            Node<int> a = new Node<int>(8,b);
+            Node<int> a0 = new Node<int>(12, a);
+            int result = Count(a0);
+            Console.WriteLine(result);
+        }
+    }
+}
